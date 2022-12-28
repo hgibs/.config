a09905dd5e2df393858d63be90a2990852b245ea
G. Holland Gibson <holland50@lokilabs.io>
2022-12-28 14:17:48 -0500

    finalizing zsh setup

--- oh-my-zsh-custom/10-main.zsh
+++ oh-my-zsh-custom/10-main.zsh
@@ -2,8 +2,6 @@
 # PATH #
 ########
 
-export BYOBU_CONFIG_DIR="~/.config/byobu"
-
 export PATH="/usr/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
@@ -12,27 +10,13 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="$HOME/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
 
 ###########
 # Exports #
 ###########
 
 export EDITOR=nano
+export BYOBU_CONFIG_DIR="~/.config/byobu"
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
@@ -49,6 +33,9 @@ export EDITOR=nano
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
+##########
+# Config #
+##########
 
 # zsh completions
 if type brew &>/dev/null; then
