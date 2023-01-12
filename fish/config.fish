98d8380847e865b5708129cf55e7282b592739bf
Holland Gibson <holland.gibson@gmail.com>
2023-01-12 11:55:49 -0500

    conda init

--- fish/config.fish
+++ fish/config.fish
@@ -88,3 +88,9 @@ if status is-interactive
     # should be at end:
     starship init fish | source
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/hollandgibson/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
