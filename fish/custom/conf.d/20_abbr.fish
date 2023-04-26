2a0ba2b197a5ea474159428b46b5d84518ebcab2
Holland Gibson <holland@hollandgibson.com>
2023-04-26 12:40:35 -0400

    adjusting exa abbrs

--- fish/custom/conf.d/20_abbr.fish
+++ fish/custom/conf.d/20_abbr.fish
@@ -43,8 +43,9 @@ abbr --add tree3 'tree -L 3'
 # abbr --add git ssh="  git remote set-url origin git@github.com:$(    git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
 abbr --add kga 'kubectl get all,cm,secret,ing,pvc,cert' #,certificaterequest'
 
-abbr --add l "exa -l"
-abbr --add ls "exa -lag --header"
+abbr --add l "exa -l --header --git --time-style=iso"
+abbr --add ls "exa -la --header --giti --time-style=long-iso"
+abbr --add lsg "exa -lag --header --git --time-style=long-iso"
 
 abbr --add buu "brew update && brew upgrade"
 
