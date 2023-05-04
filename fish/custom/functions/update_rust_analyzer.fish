59842e00284819aca98c6fa5a53651addc45f4e1
Holland Gibson <holland@hollandgibson.com>
2023-05-04 11:00:17 -0400

    making more functions backwards compatible
    (cherry picked from commit 858d9f36b17732e79110d19c06c3e9e6a2d6426b)

--- fish/custom/functions/update_rust_analyzer.fish
+++ fish/custom/functions/update_rust_analyzer.fish
@@ -1,5 +1,5 @@
 function update_rust_analyzer --description 'Updates rust-analyzer to latest release'
-	set -l ra_path (which rust-analyzer)
+	set -l ra_path "$(which rust-analyzer)"
 	echo -e "Version before:\n" (rust-analyzer --version) "\n"
 	rm -f $ra_path
 	curl -L --silent https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-aarch64-apple-darwin.gz | gunzip -c - > $ra_path
