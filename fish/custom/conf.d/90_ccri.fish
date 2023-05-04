a2a73abccd71d02b5dc592b3bd5d3a5dc9777904
Holland Gibson <holland@hollandgibson.com>
2023-05-04 17:54:48 -0400

    adding ccri stub

---
+++ fish/custom/conf.d/90_ccri.fish
@@ -0,0 +1,13 @@
+command -v ip >/dev/null 2>&1 || exit 0
+
+# if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59" 
+if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "ac:91:a1:14:29:d5"
+	set -g HOST_ENV_SETTING ccri
+
+
+
+	# >>> conda initialize >>>
+	# !! Contents within this block are managed by 'conda init' !!
+	# eval /Users/hollandgibson/.miniforge3/bin/conda "shell.fish" "hook" $argv | source
+	# <<< conda initialize <<<
+end
