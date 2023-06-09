35b8b563f0db1ca07d941dd6ac55fcc926e37af6
Holland Gibson <holland@hollandgibson.com>
2023-06-09 12:59:21 -0400

    fixed imports from legacy

---
+++ fish/custom/conf.d/05_path.fish
@@ -0,0 +1,22 @@
+# fish_add_path -a 
+
+if test (uname -o) = "Darwin"
+	# prepend homebrew and my tools first
+	fish_add_path -p /usr/local/sbin
+	fish_add_path -p /opt/homebrew/bin 
+	fish_add_path -p /opt/homebrew/sbin
+	fish_add_path -p $HOME/Applications/bin
+
+	# rest of paths
+	fish_add_path -a '/Applications/Sublime Text.app/Contents/SharedSupport/bin'
+	fish_add_path -a $HOME/.cargo/bin
+	fish_add_path -a /opt/homebrew/opt/mysql-client/bin
+
+	# mainly for fisher install
+	fish_add_path -a $HOME/.local/bin
+
+else if test (uname -o) = "GNU/Linux"
+	fish_add_path -p $HOME/bin
+end
+
+# /opt/homebrew/opt/libpq/bin 
