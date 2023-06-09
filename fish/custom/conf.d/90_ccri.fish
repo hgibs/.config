8f74c6da46b94ed592e74ebefb3e984de1335204
Holland Gibson <holland@hollandgibson.com>
2023-06-09 12:42:44 -0400

    Fixed the starship config issue

--- fish/custom/conf.d/90_ccri.fish
+++ fish/custom/conf.d/90_ccri.fish
@@ -4,7 +4,7 @@ command -v ip >/dev/null 2>&1 || exit 0
 if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "ac:91:a1:14:29:d5"
 	set -g HOST_ENV_SETTING ccri
 
-	set -g STARSHIP_CONFIG ~/dotfiles/starship.toml
+	set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
 	
 	abbr -e ls # wait for exa to become available
 	
