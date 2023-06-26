86591581937895221fc35af5967b69b8684ea6d4
Holland Gibson <holland@hollandgibson.com>
2023-06-26 15:45:14 -0400

    tweaks

--- fish/custom/functions/copy.fish
+++ fish/custom/functions/copy.fish
@@ -1,23 +1,23 @@
 function copy
     if test (uname -o) = "Darwin"
         pbcopy $argv
-        exit $status
+        return $status
     else if test (uname -o) = "GNU/Linux"
         command -q xclip
         set -l xclip_status $status
         if test $xclip_status -eq 0
             xclip $argv
-            exit $status
+            return $status
         end
 
         command -q wl-copy
         set -l wlcopy_status $status
         if test $wlcopy_status -eq 0
             wl-copy $argv
-            exit $status
+            return $status
         end
     end
 
     echo "unknown copy mapping" 1>&2
-    exit 1
+    return 1
 end
