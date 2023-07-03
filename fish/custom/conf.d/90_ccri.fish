ce9d09149d25a530ca36839bdb8d625a28d6b8ec
Holland Gibson <holland@hollandgibson.com>
2023-07-03 14:58:01 -0400

    added panic handling if interface doesn't exist

--- fish/custom/conf.d/90_ccri.fish
+++ fish/custom/conf.d/90_ccri.fish
@@ -1,5 +1,10 @@
 command -v ip >/dev/null 2>&1 || exit 0
 
+ip link show | grep -q enp0s31f6
+if test $status -ne 0
+  exit 0
+end 
+
 # if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59" 
 if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "ac:91:a1:14:29:d5"
 	set -g HOST_ENV_SETTING ccri
