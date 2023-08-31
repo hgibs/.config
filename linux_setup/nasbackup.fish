380e9c6d06264e30c9a3b55361c226e3d1bf2126
Holland Gibson <holland@hollandgibson.com>
2023-08-31 14:09:15 -0400

    adding more linux setup

--- linux_setup/nasbackup.fish
+++ linux_setup/nasbackup.fish
@@ -9,9 +9,15 @@
 #   /home/hgibson/ \
 #   -e ssh hgibson@nas0.ccri.com:/mnt/tank/backups/hgibson/laptop/main/
 
+set -l basepath "$HOME/nas0_tank/backups/hgibson/laptop"
+
+if test ! -e "$basepath"
+  mountnas
+end
 
 # rolling backups, in reverse order
-set existing_rolling_dates (ssh hgibson@nas0.ccri.com 'ls -1 -r /mnt/tank/backups/hgibson/laptop' | grep 'laptop-rolling' | grep -Eo '20[0-9]{2}-[0-9]{2}-[0-9]{2}')
+# set existing_rolling_dates (ssh hgibson@nas0.ccri.com "ls -1 -r $basepath" | grep 'laptop-rolling' | grep -Eo '20[0-9]{2}-[0-9]{2}-[0-9]{2}')
+set existing_rolling_dates (ls -1 -r "$basepath" | grep 'laptop-rolling' | grep -Eo '20[0-9]{2}-[0-9]{2}-[0-9]{2}')
 
 # if [ -z "$existing_rolling_dates" ]; then
 #   echo "No rolling backups found!"
@@ -28,24 +34,32 @@ end
 echo $count
 echo $oldest_backup
 
-if test $count -ge 3;
-  # add new backup
-  set -l output_dirname (date +%F)
-  set -l output_path "/mnt/tank/backups/hgibson/laptop/laptop-rolling-$output_dirname/"
-  set -l already_exists (ssh hgibson@nas0.ccri.com "bash -c 'test -d $output_path; echo \$?'")
+# add new backup
+set -l output_dirname (date +%F)
+set -l output_path "$basepath/laptop-rolling-$output_dirname/"
+# set -l already_exists (ssh hgibson@nas0.ccri.com "bash -c 'test -d $output_path; echo \$?'")
 
-  if test "$already_exists" -eq 0;
-    echo "$output_path already exists, stopping."
-    exit 1
-  end
-
-  echo "Rolling backup to $output_path"
-
-  rsync -a \
-    --progress \
-    --exclude="lost+found" \
-    --exclude=".cache"
-    --exclude=".m2/repository" \
-    /home/hgibson/ \
-    -e ssh hgibson@nas0.ccri.com:$output_path
+# if test "$already_exists" -eq 0;
+if test -d "$output_path"
+  echo "$output_path already exists, stopping."
+  exit 1
 end
+
+echo "Rolling backup to $output_path"
+
+rsync -a \
+  --progress \
+  --exclude="lost+found" \
+  --exclude="nas0_tank" \
+  --exclude=".tldr" \
+  --exclude=".local/lib" \
+  --exclude=".cache" \
+  --exclude=".m2/repository" \
+  /home/hgibson/ \
+  $output_path
+
+if test $count -ge 3;
+  # remove oldest backup
+  echo "Would remove $oldest_backup"
+end
+
