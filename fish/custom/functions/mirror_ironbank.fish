b61e5d771d184a81ee9c8c4b024764c859c1872d
Holland Gibson <holland@hollandgibson.com>
2023-08-31 14:11:18 -0400

    adding new functions

--- fish/custom/functions/mirror_ironbank.fish
+++ fish/custom/functions/mirror_ironbank.fish
@@ -1,29 +1,48 @@
-function mirror_ironbank
-    argparse --name=mirror_ironbank --min-args 1 'h/help' -- $argv
-    or return 1
+if test "$HOST_ENV_SETTING" = ccri
+  function mirror_ironbank
+    # docker pull --platform linux/amd64 "$argv[1]"
+    set -l oldtag (string replace "docker pull " "" "$argv")
+    # echo "docker pull $oldtag"
+    docker pull "$oldtag"
+    # set -l NEW_TAG artifactory.rtenclave.io/docker-public-local/$argv[1]
+    set -l newtag (string replace "registry1.dso.mil" "docker-art.ccri.com" "$oldtag")
+    echo "$oldtag -> $newtag"
+    docker tag $oldtag $newtag
+    docker push $newtag
 
-    if set -q _flag_help
-        echo "Usage: mirror_ironbank [image tag]\nExample: mirror_ironbank registry1.dso.mil/ironbank/opensource/apache/tomcat-openjdk8:10.1.7"
-        return 1
+    set -l PUSH_STATUS $status
+    echo "\n"
+
+    if test $PUSH_STATUS -eq 0
+      set -l K8S_IMG (docker inspect --format='{{index .RepoDigests 0}}' "$newtag")
+      echo "The new docker image:"
+      echo "$K8S_IMG"
+      echo "$newtag"
+    else
+      echo 'Push failed!! Please log in and try again.'
+      echo "docker login artifactory.rtenclave.io"
     end
-
-    if not set -q argv[1]
-        echo "Expected an argument"
-        return 1
-    end
-    
-    string match -q 'registry1.dso.mil*' $argv[1]
-    if test $status -ne 0
-        echo "Please supply a tag starting with registry1.dso.mil"
-        return 1
-    end
-
-    set -l rest_of_parts (string split 'registry1.dso.mil/' $argv[1])
-    set -l ccri_tag "docker-art.ccri.com/$rest_of_parts[2]"
-
-    set -l fish_trace 1
-   
-    docker pull $argv[1]
-    docker tag $argv[1] $ccri_tag
-    docker push $ccri_tag
+  end
 end
+
+## BASH style:
+# push_artifactory () {
+#   docker pull --platform linux/amd64 "$1"
+
+#   NEW_TAG="artifactory.rtenclave.io/docker-public-local/$1"
+#   docker tag $1 $NEW_TAG
+#   docker push $NEW_TAG
+#   STATUS=$?
+
+#   echo "\n"
+
+#   if [ "$STATUS" -eq 0 ]; then
+#     K8S_IMG=$(docker inspect --format='{{index .RepoDigests 0}}' "$NEW_TAG")
+
+#     echo "To use this image in a manifest, refer to its shasum, i.e.:"
+#     echo "image: $K8S_IMG"
+#   else
+#     echo 'Push failed!! Please log in and try again.'
+#     echo "docker login artifactory.rtenclave.io"
+#   fi
+# }
