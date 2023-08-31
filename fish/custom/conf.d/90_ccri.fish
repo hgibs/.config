7b7843f7aa0d4417dedfa4b052e55ed3d1fbef7a
Holland Gibson <holland@hollandgibson.com>
2023-08-31 14:19:50 -0400

    ccri changes

--- fish/custom/conf.d/90_ccri.fish
+++ fish/custom/conf.d/90_ccri.fish
@@ -15,16 +15,27 @@ if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | h
 	
 	# abbr -e ls # wait for exa to become available
 	
+	abbr record_container 'asciinema rec ~/terminal_recording/sessions/(date +%FT%T).asc -c "zellij --layout ~/terminal_recording/recording_stack_layout_container.kdl"'
+	abbr record_crush 'asciinema rec ~/terminal_recording/sessions/(date +%FT%T).asc -c "zellij --layout ~/terminal_recording/recording_stack_layout_crusher.kdl"'
 	abbr record_cmcc 'asciinema rec ~/terminal_recording/sessions/(date +%FT%T).asc -c "zellij --layout ~/terminal_recording/recording_stack_layout.kdl"'
 	abbr mci 'mvn clean install'
 	abbr mcis 'mvn clean install -DskipTests'
 	abbr mdt 'mvn dependency:tree'
+	abbr mdtf 'mvn dependency:tree -DoutputFile=dependencies.txt'
 	abbr mst 'mvn -Dmaven.test.skip=true'
 	abbr m 'mvn'
 	abbr mt 'mvn test'
+	abbr mc 'mvn clean'
 	abbr msc 'mvn scala:console'
+	abbr mep 'mvn help:effective-pom -Dverbose=true'
+	abbr ml 'mvn fr.jcgay.maven.plugins:buildplan-maven-plugin:list'
 	abbr mountnas 'sshfs hgibson@nas0.ccri.com:/mnt/tank $HOME/nas0_tank'
 	abbr mountlxc 'sshfs hgibson@hgibson-lxc.ccri.com:/home/hgibson $HOME/lxc'
+	
+	abbr crusher '$HOME/Developer/optix_edge/deployments/crusher'
+	abbr cmcc '$HOME/Developer/optix_edge/deployments/cmcc'
+	abbr java-utils '$HOME/Developer/optix_edge/java-utils'
+	abbr containers '$HOME/Developer/optix_edge/containers'
 	
 	# colorful less!
 	set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
@@ -35,14 +46,25 @@ if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | h
 	set -gx MAVEN_OPTS '-Xmx1500M -Xms1G -Duser.timezone=UTC -Xss32m'
 	set -gx JAVA_HOME "/usr/lib/jvm/java-8-openjdk-amd64"
 
-	fish_add_path -a "$HOME/src/inrev"
+	fish_add_path --append "$HOME/src/inrev"
 	
 	# add coursier to path
-	fish_add_path -p "$HOME/.local/share/coursier/bin"
+	fish_add_path --prepend "$HOME/.local/share/coursier/bin"
 
 	# add pip to path
-	fish_add_path -p "$HOME/.local/bin"
+	fish_add_path --prepend "$HOME/.local/bin"
 	
+	# add spotbugs to end of path
+	# set -gxa PATH "$HOME/Developer/spotbugs-utils"
+	fish_add_path --global --append "$HOME/Developer/spotbugs-utils"
+
+	#default to java 8
+	set -gx JAVA_HOME "/usr/lib/jvm/java-8-openjdk-amd64"
+    set -a PATH "$JAVA_HOME/bin"
+	
+	
+	# add NVM setup
+	set -gx NVM_DIR "$HOME/.nvm"
 	# >>> conda initialize >>>
 	# !! Contents within this block are managed by 'conda init' !!
 	# eval /Users/hollandgibson/.miniforge3/bin/conda "shell.fish" "hook" $argv | source
