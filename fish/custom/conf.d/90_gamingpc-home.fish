aa9322dd4995129574e88c4d216439edbc5b0e15
holland <holland@gamingpc.home.hollandgibson.com>
2024-02-28 16:31:33 -0500

    Add gamingpc updates

--- fish/custom/conf.d/90_gamingpc-home.fish
+++ fish/custom/conf.d/90_gamingpc-home.fish
@@ -2,29 +2,29 @@ command -v ip >/dev/null 2>&1 || exit 0
 
 ip link show | grep -q enp9s0
 if test $status -ne 0
-  exit 0
-end 
+    exit 0
+end
 
 # if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59"
 if test (ip -o link show enp9s0 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "9c:6b:00:1d:ad:d4"
-	set -g HOST_ENV_SETTING gamingpc-home
+    set -g HOST_ENV_SETTING gamingpc-home
 
-	set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
-	set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
+    set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
+    set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
 
-	# colorful less!
-	set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+    # colorful less!
+    set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 
-	# move asciinema config (which disables automatic uploading)
-	set -gx ASCIINEMA_CONFIG_HOME "$HOME/dotfiles/asciinema"
+    # move asciinema config (which disables automatic uploading)
+    set -gx ASCIINEMA_CONFIG_HOME "$HOME/dotfiles/asciinema"
 
-	fish_add_path -p "$HOME/.local/bin"
-	fish_add_path -a "$HOME/.cargo/bin"
+    fish_add_path -p "$HOME/.local/bin"
+    fish_add_path -a "$HOME/.cargo/bin"
 
-	# enable byobu
-	set -g BYOBU_ENABLE "true"
-	# >>> conda initialize >>>
-	# !! Contents within this block are managed by 'conda init' !!
-	# eval /Users/hollandgibson/.miniforge3/bin/conda "shell.fish" "hook" $argv | source
-	# <<< conda initialize <<<
+    # enable byobu
+    set -g BYOBU_ENABLE true
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    eval /Users/hollandgibson/.miniforge3/bin/conda "shell.fish" hook $argv | source
+    # <<< conda initialize <<<
 end
