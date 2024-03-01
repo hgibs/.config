18d9b0596f66f4624b029567019bb2743f1407fc
Holland Gibson <holland@hollandgibson.com>
2024-02-29 22:58:49 -0500

    Using faster, universal variable instead of always running ifconfig/ip

--- fish/custom/conf.d/90_gamingpc-home.fish
+++ fish/custom/conf.d/90_gamingpc-home.fish
@@ -1,12 +1,13 @@
-command -v ip >/dev/null 2>&1 || exit 0
+# command -v ip >/dev/null 2>&1 || exit 0
 
-ip link show | grep -q enp9s0
-if test $status -ne 0
-    exit 0
-end
+# ip link show | grep -q enp9s0
+# if test $status -ne 0
+#     exit 0
+# end
 
-# if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59"
-if test (ip -o link show enp9s0 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "9c:6b:00:1d:ad:d4"
+# # if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59"
+# if test (ip -o link show enp9s0 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "9c:6b:00:1d:ad:d4"
+if test "$_identifier" = "9c:6b:00:1d:ad:d4"
     set -g HOST_ENV_SETTING gamingpc-home
 
     set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
--- fish/custom/conf.d/90_tower-home.fish
+++ fish/custom/conf.d/90_tower-home.fish
@@ -1,12 +1,13 @@
-command -v ip >/dev/null 2>&1 || exit 0
+# command -v ip >/dev/null 2>&1 || exit 0
 
-ip link show | grep -q enp0s31f6
-if test $status -ne 0
-  exit 0
-end 
+# ip link show | grep -q enp0s31f6
+# if test $status -ne 0
+#   exit 0
+# end 
 
-# if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59" 
-if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "e0:d5:5e:21:8e:72"
+# # if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59" 
+# if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "e0:d5:5e:21:8e:72"
+if test "$_identifier" = "e0:d5:5e:21:8e:72"
 	set -g HOST_ENV_SETTING tower-home
 
 	set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
