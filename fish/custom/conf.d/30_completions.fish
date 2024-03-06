22dd879a77e880ee58e25a7022e03d25336d5d2d
Holland Gibson <holland.gibson@ccri.com>
2024-03-06 17:06:47 -0500

    Found out brew does this for us!

--- fish/custom/conf.d/30_completions.fish
+++ fish/custom/conf.d/30_completions.fish
@@ -1,4 +1,8 @@
 set kubectl_file "$HOME/.config/fish/completions/kubectl.fish"
-if test ! -f "$kubectl_file"
-    kubectl completion fish > "$kubectl_file"
-end 
+
+if test (uname -o) != "Darwin"
+    # brew vendor path actually works
+    if test ! -f "$kubectl_file"
+        kubectl completion fish > "$kubectl_file"
+    end 
+end
