2f2a75d34c10f1212717e123fe93020aa59da9a7
Holland Gibson <holland@hollandgibson.com>
2024-03-18 22:01:53 -0400

    Better kubectl handling and HUNSPELL additions

--- fish/custom/conf.d/30_completions.fish
+++ fish/custom/conf.d/30_completions.fish
@@ -2,7 +2,9 @@ set kubectl_file "$HOME/.config/fish/completions/kubectl.fish"
 
 if test (uname -o) != "Darwin"
     # brew vendor path actually works
-    if test ! -f "$kubectl_file"
-        kubectl completion fish > "$kubectl_file"
-    end 
+    if command -v kubectl >/dev/null 2>&1
+        if test ! -f "$kubectl_file"
+            kubectl completion fish > "$kubectl_file"
+        end 
+    end
 end
