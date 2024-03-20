8116fab986b70c1aa30dc34f23f0c2e86ac7659b
hgibs <holland@hollandgibson.com>
2024-03-19 20:31:26 -0400

    nixos set up

--- fish/custom/conf.d/00_prelim.fish
+++ fish/custom/conf.d/00_prelim.fish
@@ -1,4 +1,4 @@
-set -gx FISH_MAJOR (fish --version | awk '{print $3}' | head -c 1)
+# set -gx FISH_MAJOR (fish --version | awk '{print $3}' | head -c 1)
 set -gx FISH_MINOR (fish --version | awk '{print $3}' | head -c 3 | tail -c 1)
 
 set fish_color_command brcyan
@@ -13,7 +13,11 @@ if set -q _identifier
  # do nothing
 else
   if command -v ip >/dev/null 2>&1
-    set -U _identifier (ip -j link show | jq -r '.[] | select(.ifname | test("en.*")) | .address')
+    if test -z "$NIX_PATH"
+      set -U _identifier (ip -j link show | jq -r '.[] | select(.ifname | test("en.*")) | .address')
+    else 
+      set -U _identifier (ip -j link show | jq -r '.[] | select(.ifname | test("e.*")) | .address')-nixos
+    end
   else if command -v ifconfig >/dev/null 2>&1
     if ifconfig -a | grep -q en0
       set -U _identifier (ifconfig en0 | grep ether | awk '{print $2}')
