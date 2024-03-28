5d395dc87f3d634044e0ba8530a19c98f6ac2c46
Holland Gibson <holland.gibson@ccri.com>
2024-03-28 10:29:12 -0400

    CCRi local changes

--- fish/custom/functions/copy.fish
+++ fish/custom/functions/copy.fish
@@ -3,18 +3,27 @@ function copy
         pbcopy $argv
         return $status
     else if test (uname -o) = "GNU/Linux"
-        command -q xclip
-        set -l xclip_status $status
-        if test $xclip_status -eq 0
-            xclip $argv
-            return $status
-        end
+        if test "$XDG_SESSION_TYPE" = "x11"
+            command -q xclip
+            set -l xclip_status $status
+            if test $xclip_status -eq 0
+                xclip $argv
+                return $status
+            end
 
-        command -q wl-copy
-        set -l wlcopy_status $status
-        if test $wlcopy_status -eq 0
-            wl-copy $argv
-            return $status
+            command -q xsel
+            set -l xsel_status $status
+            if test $xsel_status  -eq 0
+                xsel -ib $argv
+                return $status
+            end
+        else
+            command -q wl-copy
+            set -l wlcopy_status $status
+            if test $wlcopy_status -eq 0
+                wl-copy $argv
+                return $status
+            end
         end
     end
 
