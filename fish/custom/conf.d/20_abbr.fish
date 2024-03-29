044b84b466de49177a783d56a279a74ada1ff108
Holland Gibson <holland.gibson@ccri.com>
2024-03-29 14:20:01 -0400

    Added weather abbr
    (cherry picked from commit 81c5e5294f2e89ed9502ad27f6b6d61edd9f0124)

--- fish/custom/conf.d/20_abbr.fish
+++ fish/custom/conf.d/20_abbr.fish
@@ -3,6 +3,55 @@
 ###########
 
 ## core utils replacements
+abbr --add zshconfig "subl ~/.zshrc"
+abbr --add sshconfig "subl ~/.ssh/config"
+abbr --add wanip 'dig @resolver4.opendns.com myip.opendns.com +short'
+abbr --add tg "terragrunt"
+abbr --add tf "terraform"
+abbr --add tgaa "terragrunt apply --auto-approve"
+abbr --add tgra "terragrunt run-all apply"
+
+abbr --add weather "curl wttr.in/Charlottesville"
+
+abbr --add clippy_noboiler 'cargo clippy --fix -- -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used -W clippy::expect_used'
+abbr --add reset_fish 'source ~/.config/fish/config.fish && gpgconf --kill gpg-agent'
+
+abbr --add t 'tldr'
+# abbr --add m '/usr/bin/man'
+abbr --add p 'ping -o'
+
+abbr --add md 'mkdir -p'
+
+abbr --add randtext "$RANDOM_SOURCE '[:alnum:]' 64"
+abbr --add randprint "$RANDOM_SOURCE '[:print:]' 64"
+abbr --add randpwd "$RANDOM_SOURCE '[:graph:]' 64"
+abbr --add randnum "$RANDOM_SOURCE '[:digit:]' 32"
+abbr --add randhex "openssl rand -base64 16 | md5sum | head -c24; echo"
+abbr --add randomsym 'cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 32; echo'
+
+abbr --add gp_ssl 'GIT_SSL_NO_VERIFY=1 git push'
+abbr --add gg 'br --conf ~/.config/broot/git-diff-conf.toml --git-status'
+
+# abbr --add godu '/opt/homebrew/opt/gdu/bin/gdu'
+abbr --add godu '/opt/homebrew/Cellar/gdu/5.*/bin/gdu'
+
+# abbr --add ohmyzsh "mate ~/.oh-my-zsh"
+# abbr --add tf "terraform"
+
+abbr --add tree2 'tree -L 2'
+abbr --add tree3 'tree -L 3'
+
+# abbr --add git https="git remote set-url origin 'https://lokilabs@dev.azure.com/lokilabs/Loki%20Datascience/_git/Loki_Datascience'"
+# abbr --add git https="git remote set-url origin https://github.com/$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+# abbr --add git ssh="  git remote set-url origin git@github.com:$(    git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+
+abbr --add kga 'kubectl get all,cm,secret,ing,pvc,cert' #,certificaterequest'
+
+# abbr --add psf "ps --format '{{.Names}}{{.Status}}' | column -t -s ' '"
+abbr --add dps "docker_ps_pretty"
+abbr --add watchf "watch -dcx $FISH -c"
+abbr --add watchdps "watch -dcx $FISH -c docker_ps_pretty"
+
 abbr --add l "eza -l --header --git --time-style=iso"
 abbr --add md 'mkdir -p'
 abbr --add t 'tldr'
