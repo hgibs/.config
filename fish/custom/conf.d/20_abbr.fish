a04bb907c2abce034e09ed12d30c8db6b60ad427
Holland Gibson <holland.gibson@ccri.com>
2024-06-04 14:01:27 -0400

    minot changes and some bug fixes
    (cherry picked from commit 5942ed2413766693e0bf973b7ea64f9679753481)
    (cherry picked from commit 0b548da480f4f65d5bef17849b0f202c7c1a2581)

--- fish/custom/conf.d/20_abbr.fish
+++ fish/custom/conf.d/20_abbr.fish
@@ -56,8 +56,8 @@ abbr --add hia 'helmfile --interactive apply'
 abbr --add ha 'helmfile apply'
 
 # abbr --add psf "ps --format '{{.Names}}{{.Status}}' | column -t -s ' '"
-abbr --add dps docker_ps_pretty
 abbr --add dr "docker run --rm -it"
+abbr --add dps docker_ps_pretty
 abbr --add watchf "watch -dcx $FISH -c"
 abbr --add watchdps "watch -dcx $FISH -c docker_ps_pretty"
 
@@ -84,6 +84,8 @@ switch (uname -o)
 end
 
 abbr --add !! --position anywhere --function last_history_item
+abbr --add buu "brew update && brew upgrade"
+abbr --add sauu "sudo apt update && sudo apt upgrade -y"
 
 abbr --add tree2 'tree -L 2'
 abbr --add tree3 'tree -L 3'
@@ -116,7 +118,48 @@ abbr --add gcpc 'git cherry-pick --continue'
 abbr --erase gcp
 abbr --add gcp 'git cherry-pick -x'
 abbr --add rm_orig 'find . -name \*.orig -delete'
+# abbr --add git https="git remote set-url origin https://github.com/$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+# abbr --add git ssh="  git remote set-url origin git@github.com:$(    git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+# abbr --add git https="git remote set-url origin 'https://lokilabs@dev.azure.com/lokilabs/Loki%20Datascience/_git/Loki_Datascience'"
+abbr --add gp_ssl 'GIT_SSL_NO_VERIFY=1 git push'
+
+## Random Generators
+abbr --add randhex "openssl rand -base64 16 | md5 | head -c24; echo"
+abbr --add randnum "$RANDOM_SOURCE '[:digit:]' 32"
+abbr --add randomsym 'cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 32; echo'
+abbr --add randprint "$RANDOM_SOURCE '[:print:]' 64"
+abbr --add randpwd "$RANDOM_SOURCE '[:graph:]' 64"
+abbr --add randtext "$RANDOM_SOURCE '[:alnum:]' 64"
+
+
+# abbr --add m '/usr/bin/man'
+# abbr --add tf "terraform"
+abbr --add dps docker_ps_pretty
+abbr --add gg 'br --conf ~/.config/broot/git-diff-conf.toml --git-status'
+abbr --add godu '/opt/homebrew/Cellar/gdu/5.*/bin/gdu'
+abbr --add kga 'kubectl get all,cm,secret,ing,pvc,cert' #,certificaterequest'
+abbr --add reset_fish 'source ~/.config/fish/config.fish && gpgconf --kill gpg-agent'
+abbr --add sshconfig "subl ~/.ssh/config"
+# terragrunt
+abbr --add tf terraform
+abbr --add tg terragrunt
+abbr --add tgaa "terragrunt apply --auto-approve"
+abbr --add tgra "terragrunt run-all apply"
+
+
+## yt-dlp
+abbr --add --position anywhere -- ytplaylist "-o \"%(playlist_index&{} - |)s%(title)s.%(ext)s\""
+
+# abbr --add git https="git remote set-url origin https://github.com/$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+# abbr --add git ssh="  git remote set-url origin git@github.com:$(    git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+# abbr --add git https="git remote set-url origin 'https://lokilabs@dev.azure.com/lokilabs/Loki%20Datascience/_git/Loki_Datascience'"
+
+# abbr --add m '/usr/bin/man'
+# abbr --add tf "terraform"
+
+# git abbrs
 abbr --add gfa 'git fetch --all --prune --tags'
 abbr --add gstaa 'git stash --all'
 abbr --add gbl 'git blame -b -w -C -C -C'
 abbr --add gbu 'git branch -vv --set-upstream-to origin/(__git.current_branch) (__git.current_branch)'
+abbr --add gd 'git difftool'
