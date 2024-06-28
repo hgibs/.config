807ada9cc43154df8950fbc00f4089b3f508743c
Holland Gibson <holland.gibson@ccri.com>
2024-06-28 15:11:02 -0400

    Adding go bin and formatting

--- fish/custom/conf.d/05_path.fish
+++ fish/custom/conf.d/05_path.fish
@@ -1,26 +1,28 @@
 # fish_add_path -a 
 
-if test (uname -o) = "Darwin"
-	# prepend homebrew and my tools first
-	fish_add_path --prepend /usr/local/sbin
-	fish_add_path --prepend /opt/homebrew/bin 
-	fish_add_path --prepend /opt/homebrew/sbin
-	fish_add_path --prepend $HOME/Applications/bin
+if test (uname -o) = Darwin
+    # prepend homebrew and my tools first
+    fish_add_path --prepend /usr/local/sbin
+    fish_add_path --prepend /opt/homebrew/bin
+    fish_add_path --prepend /opt/homebrew/sbin
+    fish_add_path --prepend $HOME/Applications/bin
 
-	# rest of paths
-	fish_add_path --append '/Applications/Sublime Text.app/Contents/SharedSupport/bin'
-	fish_add_path --append $HOME/.cargo/bin
-	fish_add_path --append /opt/homebrew/opt/mysql-client/bin
+    # rest of paths
+    fish_add_path --append '/Applications/Sublime Text.app/Contents/SharedSupport/bin'
+    fish_add_path --append $HOME/.cargo/bin
+    fish_add_path --append /opt/homebrew/opt/mysql-client/bin
 
 
-else if test (uname -o) = "GNU/Linux"
-	fish_add_path --prepend $HOME/bin
-	fish_add_path --append $HOME/.cargo/bin
-	fish_add_path --append $HOME/node/bin
+else if test (uname -o) = GNU/Linux
+    fish_add_path --prepend $HOME/bin
+    fish_add_path --append $HOME/.cargo/bin
+    fish_add_path --append $HOME/node/bin
+    fish_add_path --append $HOME/go/bin
 end
+
+# could switch to `test -d` type checks
 
 # mainly for fisher install
 fish_add_path --append $HOME/.local/bin
 
 # /opt/homebrew/opt/libpq/bin 
-
