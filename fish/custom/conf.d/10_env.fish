08cc654fd42b320df50bb68a65539b3bbb5afaa6
Holland Gibson <holland@hollandgibson.com>
2024-07-05 14:44:55 -0400

    Configs

--- fish/custom/conf.d/10_env.fish
+++ fish/custom/conf.d/10_env.fish
@@ -4,19 +4,13 @@
 
 set -gx EDITOR hx
 set -gx BYOBU_CONFIG_DIR ~/.config/byobu
-
 set -gx LESSOPEN "| /System/Volumes/Data/opt/homebrew/bin/src-hilite-lesspipe.sh %s"
 set -gx LESS ' -R '
-
-set -gx PAGER 'less'
-
-set -gx RANDOM_SOURCE 'urandom'
-
+set -gx PAGER less
+set -gx RANDOM_SOURCE urandom
 set -gx ASCIINEMA_CONFIG_HOME "$HOME/dotfiles/asciinema"
-
+set -gx LANG "en_US.UTF-8"
 
 ###############
 # Non-exports #
 ###############
-
-
--- fish/custom/conf.d/30_completions.fish
+++ fish/custom/conf.d/30_completions.fish
@@ -1,10 +1,12 @@
 set kubectl_file "$HOME/.config/fish/completions/kubectl.fish"
 
-if test (uname -o) != "Darwin"
+if test (uname -o) != Darwin
     # brew vendor path actually works
     if command -v kubectl >/dev/null 2>&1
         if test ! -f "$kubectl_file"
-            kubectl completion fish > "$kubectl_file"
-        end 
+            kubectl completion fish >"$kubectl_file"
+        end
     end
 end
+
+fzf --fish | source
