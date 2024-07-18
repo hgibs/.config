6337847a94dc0081cb47b99aafa524ff6835ce9d
Holland Gibson <holland.gibson@ccri.com>
2024-07-18 10:27:30 -0400

    demo tweaks

--- fish/custom/functions/inspire.fish
+++ fish/custom/functions/inspire.fish
@@ -1,7 +1,7 @@
 function inspire -d "Choose an inspiring quote from random"
-    set -q INSPIRE_QUOTE_JSON_FILE; or set INSPIRE_QUOTE_JSON_FILE "quotes.json"
+    set -q INSPIRE_QUOTE_JSON_FILE; or set INSPIRE_QUOTE_JSON_FILE "$HOME/dotfiles/quotes.json"
 
-    if command -vq "jq"
+    if command -vq jq
         jq --version >/dev/null
     else
         echo "'jq' command not found, please install it and add it to the \$PATH" >&2
@@ -14,7 +14,7 @@ function inspire -d "Choose an inspiring quote from random"
         set -l index (shuf -i 0-$last_index -n 1)
         set -l quote (jq -r ".[$index].text" "$INSPIRE_QUOTE_JSON_FILE")
         set -l author (jq -r ".[$index].author" "$INSPIRE_QUOTE_JSON_FILE")
-        printf "\"%s\"\n - %s\n" "$quote" "$author" 
+        printf "\"%s\"\n - %s\n" "$quote" "$author"
     else
         echo "Could not find 'quotes.json' file. Please set the environment variable INSPIRE_QUOTE_JSON_FILE to the path to the desired quotes json file." >&2
         return 1
