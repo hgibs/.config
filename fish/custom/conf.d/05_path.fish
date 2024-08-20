c6938abce703439f31fa01c01a81a9edc5a475e9
Holland Gibson <holland@hollandgibson.com>
2024-08-19 23:15:49 -0400

    Created new update function
    (cherry picked from commit 0a10b0efde69ea60ce4da4efd3bc7c54281f2592)

--- fish/custom/conf.d/05_path.fish
+++ fish/custom/conf.d/05_path.fish
@@ -1,5 +1,8 @@
 # fish_add_path -a 
 
+fish_add_path -p "$HOME/.local/bin"
+fish_add_path --prepend $HOME/bin
+
 if test (uname -o) = Darwin
     # prepend homebrew and my tools first
     fish_add_path --prepend /usr/local/sbin
@@ -13,7 +16,6 @@ if test (uname -o) = Darwin
     fish_add_path --append /opt/homebrew/opt/mysql-client/bin
 
 else if test (uname -o) = GNU/Linux
-    fish_add_path --prepend $HOME/bin
     fish_add_path --append $HOME/.cargo/bin
     fish_add_path --append $HOME/node/bin
     fish_add_path --append $HOME/go/bin
