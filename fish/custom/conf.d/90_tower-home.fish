bb082f072495b8cdf5b51890066df0885d1b1db8
Holland Gibson <holland@hollandgibson.com>
2024-11-29 21:42:14 -0500

    lots of tweaks

--- fish/custom/conf.d/90_tower-home.fish
+++ fish/custom/conf.d/90_tower-home.fish
@@ -8,30 +8,31 @@
 # # if test (ifconfig en0 | grep ether | awk '{print $2}') = "f8:4d:89:69:83:59" 
 # if test (ip -o link show enp0s31f6 | grep -oE "([a-f0-9]{2}:){5}[a-f0-9]{2}" | head -n 1) = "e0:d5:5e:21:8e:72"
 if test "$_identifier" = "e0:d5:5e:21:8e:72"
-	set -g HOST_ENV_SETTING tower-home
+    set -g HOST_ENV_SETTING tower-home
 
-	set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
-	set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
+    set -gx HELIX_RUNTIME $HOME/.config/helix/runtime
+    set -gx STARSHIP_CONFIG ~/dotfiles/starship.toml
 
-	# colorful less!
-	set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+    # colorful less!
+    set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 
-	# move asciinema config (which disables automatic uploading)
-	set -gx ASCIINEMA_CONFIG_HOME "$HOME/dotfiles/asciinema"
+    # move asciinema config (which disables automatic uploading)
+    set -gx ASCIINEMA_CONFIG_HOME "$HOME/dotfiles/asciinema"
 
-	# git credential helper
-	set -gx GCM_CREDENTIAL_STORE "cache"
+    # git credential helper
+    set -gx GCM_CREDENTIAL_STORE cache
 
-	fish_add_path -p "$HOME/.local/bin"
-	fish_add_path -a "$HOME/.cargo/bin"
-	# fish_add_path -a "$HOME/miniforge3/bin"
+    fish_add_path -p "$HOME/.local/bin"
+    fish_add_path -a "$HOME/.cargo/bin"
+    # fish_add_path -a "$HOME/miniforge3/bin"
+    if command -vq conda
+        eval $HOME/miniforge3/bin/conda "shell.fish" hook $argv | source
+    end
 
-	eval $HOME/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-
-	# enable byobu
-	set -g BYOBU_ENABLE "true"
-	# >>> conda initialize >>>
-	# !! Contents within this block are managed by 'conda init' !!
-	# eval /Users/hollandgibson/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-	# <<< conda initialize <<<
+    # enable byobu
+    set -g BYOBU_ENABLE true
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    # eval /Users/hollandgibson/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+    # <<< conda initialize <<<
 end
--- restic/backup.sh
+++ restic/backup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+date
+
 # password is in the password.secret file
-
-source password.secret
-
-restic -r sftp:macb14@nas.home.hollandgibson.com:/mnt/Saturn/macbook14/restic/teal cat config
-
-restic -r sftp:macb14@nas.home.hollandgibson.com:/mnt/Saturn/macbook14/restic/teal backup "$HOME"
+if pmset -g ps | head -n 1 | grep -q 'AC Power'; then
+  source $HOME/.config/restic/password.secret
+  /opt/homebrew/bin/restic -r sftp:macb14@nas.home.hollandgibson.com:/mnt/Saturn/macbook14/restic/teal cat config
+  /opt/homebrew/bin/restic -r sftp:macb14@nas.home.hollandgibson.com:/mnt/Saturn/macbook14/restic/teal backup "$HOME"
+else
+  printf "Must be plugged into AC power to backup\n" >&2
+fi
