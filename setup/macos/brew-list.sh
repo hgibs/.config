679f79a02d6fca6cdb95dd802faed957cc041f6e
Holland Gibson <holland@hollandgibson.com>
2025-06-20 11:42:42 -0400

    bug fix

--- setup/macos/brew-list.sh
+++ setup/macos/brew-list.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 LEAVES_FILE="/Users/hollandgibson/.config/setup/macos/brew-leaves.txt"
-printf "# created by ~/.config/setup/macos/brew-list.sh on %\n" "$(date)" > $LEAVES_FILE
+printf "# created by ~/.config/setup/macos/brew-list.sh on %s \n" "$(date)" > $LEAVES_FILE
 /opt/homebrew/bin/brew leaves >> $LEAVES_FILE
 
 CASKS_FILE="/Users/hollandgibson/.config/setup/macos/brew-casks.txt"
-printf "# created by ~/.config/setup/macos/brew-list.sh on %\n" "$(date)" > $CASKS_FILE
+printf "# created by ~/.config/setup/macos/brew-list.sh on %s \n" "$(date)" > $CASKS_FILE
 /opt/homebrew/bin/brew list --cask >> $CASKS_FILE
