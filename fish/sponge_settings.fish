87164390ac7693cb2832de9cd580d33668f16d68
Holland Gibson <holland.gibson@ccri.com>
2025-07-24 15:33:14 -0400

    chill sponge out a bit

--- fish/sponge_settings.fish
+++ fish/sponge_settings.fish
@@ -1,6 +1,6 @@
 # sponge configs https://github.com/meaningful-ooo/sponge
-set sponge_allow_previously_successful false
-set sponge_delay 5
+# set sponge_allow_previously_successful false
+set sponge_delay 15
 
 # zoxide shouldn't be in the history, that's kind of the point
 set sponge_regex_patterns (echo '^z\x20' | string unescape)
