40b37100d3b01df228e6bba15403a1e9e447c5eb
Holland Gibson <holland.gibson@ccri.com>
2025-09-09 16:40:54 -0400

    Adding rbenv support

--- fish/custom/conf.d/99_final.fish
+++ fish/custom/conf.d/99_final.fish
@@ -2,6 +2,10 @@
 
 set -gx SHELL (command -v fish)
 
+if command -v rbenv >/dev/null 2>&1
+    status --is-interactive; and ~/.rbenv/bin/rbenv init - --no-rehash fish | source
+end
+
 if command -v zoxide >/dev/null 2>&1
     zoxide init fish | source
 end
